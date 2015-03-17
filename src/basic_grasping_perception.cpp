3fdd20bbf8fdee0a4dc846fed2310e47fb2b4ada
Michael Ferguson <mfergs7@gmail.com>
2015-03-17 12:30:24 -0700

    fork from preview

---
+++ src/basic_grasping_perception.cpp
@@ -0,0 +1,224 @@
+/*
+ * Copyright 2013-2014, Unbounded Robotics Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Unbounded Robotics, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Author: Michael Ferguson
+
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <tf/transform_listener.h>
+
+#include <simple_grasping/object_support_segmentation.h>
+#include <simple_grasping/shape_grasp_planner.h>
+#include <grasping_msgs/FindGraspableObjectsAction.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+namespace simple_grasping
+{
+
+/**
+ *  \brief ROS wrapper for shape grasp planner + object support segmentation
+ */
+class BasicGraspingPerception
+{
+  typedef actionlib::SimpleActionServer<grasping_msgs::FindGraspableObjectsAction> server_t;
+
+public:
+  BasicGraspingPerception(ros::NodeHandle n) : nh_(n), debug_(false), find_objects_(false)
+  {
+    // Create planner
+    double opening, depth;
+    nh_.param("gripper_max_opening", opening, 0.09);
+    nh_.param("gripper_finger_depth", depth, 0.02);
+    planner_.reset(new ShapeGraspPlanner(opening, depth));
+
+    // use_debug: enable/disable output of a cloud containing object points
+    nh_.getParam("use_debug", debug_);
+
+    // frame_id: frame to transform cloud to (should be XY horizontal)
+    world_frame_ = "base_link";
+    nh_.getParam("frame_id", world_frame_);
+
+    // cluster_tolerance: minimum separation distance of two objects
+    double cluster_tolerance = 0.01;
+    nh_.getParam("cluster_tolerance", cluster_tolerance);
+
+    // cluster_min_size: minimum size of an object
+    int cluster_min_size = 50;
+    nh_.getParam("cluster_min_size", cluster_min_size);
+
+    // Create perception
+    segmentation_.reset(new ObjectSupportSegmentation(cluster_tolerance, cluster_min_size));
+
+    // Advertise an action for perception + planning
+    server_.reset(new server_t(nh_, "find_objects",
+                               boost::bind(&BasicGraspingPerception::executeCallback, this, _1),
+                               false));
+
+    // Publish debugging views
+    if (debug_)
+    {
+      object_cloud_pub_ = nh_.advertise< pcl::PointCloud<pcl::PointXYZRGB> >("object_cloud", 1);
+      support_cloud_pub_ = nh_.advertise< pcl::PointCloud<pcl::PointXYZRGB> >("support_cloud", 1);
+    }
+
+    // Range filter for cloud
+    range_filter_.setFilterFieldName("z");
+    range_filter_.setFilterLimits(0, 2.5);
+
+    // Subscribe to head camera cloud
+    cloud_sub_ = nh_.subscribe< pcl::PointCloud<pcl::PointXYZRGB> >("/head_camera/depth_registered/points",
+                                                                     1,
+                                                                     &BasicGraspingPerception::cloudCallback,
+                                                                     this);
+
+    // Start thread for action
+    server_->start();
+
+    ROS_INFO("basic_grasping_perception initialized");
+  }
+
+private:
+  void cloudCallback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& cloud)
+  {
+    // be lazy
+    if (!find_objects_)
+      return;
+
+    ROS_DEBUG("Cloud recieved with %d points.", static_cast<int>(cloud->points.size()));
+
+    // Filter out noisy long-range points
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
+    range_filter_.setInputCloud(cloud);
+    range_filter_.filter(*cloud_filtered);
+    ROS_DEBUG("Filtered for range, now %d points.", static_cast<int>(cloud_filtered->points.size()));
+
+    // Transform to grounded
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZRGB>);
+    if (!pcl_ros::transformPointCloud(world_frame_, *cloud_filtered, *cloud_transformed, listener_))
+    {
+      ROS_ERROR("Error transforming to frame %s", world_frame_.c_str());
+      return;
+    }
+
+    // Run segmentation
+    objects_.clear();
+    supports_.clear();
+    pcl::PointCloud<pcl::PointXYZRGB> object_cloud;
+    pcl::PointCloud<pcl::PointXYZRGB> support_cloud;
+    if (debug_)
+    {
+      object_cloud.header.frame_id = cloud_transformed->header.frame_id;
+      support_cloud.header.frame_id = cloud_transformed->header.frame_id;
+    }
+    segmentation_->segment(cloud_transformed, objects_, supports_, object_cloud, support_cloud, debug_);
+
+    if (debug_)
+    {
+      object_cloud_pub_.publish(object_cloud);
+      support_cloud_pub_.publish(support_cloud);
+    }
+
+    // Ok to continue processing
+    find_objects_ = false;
+  }
+
+  void executeCallback(const grasping_msgs::FindGraspableObjectsGoalConstPtr& goal)
+  {
+    grasping_msgs::FindGraspableObjectsResult result;
+
+    // Get objects
+    find_objects_ = true;
+    ros::Time t = ros::Time::now();
+    while (find_objects_ == true)
+    {
+      ros::Duration(1/50.0).sleep();
+      if (ros::Time::now() - t > ros::Duration(3.0))
+      {
+        find_objects_ = false;
+        server_->setAborted(result, "Failed to get camera data in alloted time.");
+        ROS_ERROR("Failed to get camera data in alloted time.");
+        return;
+      }
+    }
+
+    // Set object results
+    for (size_t i = 0; i < objects_.size(); ++i)
+    {
+      grasping_msgs::GraspableObject g;
+      g.object = objects_[i];
+      if (goal->plan_grasps)
+      {
+        // Plan grasps for object
+        planner_->plan(objects_[i], g.grasps);
+      }
+      result.objects.push_back(g);
+    }
+    // Set support surfaces
+    result.support_surfaces = supports_;
+
+    server_->setSucceeded(result, "Succeeded.");
+  }
+
+  ros::NodeHandle nh_;
+
+  bool debug_;
+
+  tf::TransformListener listener_;
+  std::string world_frame_;
+
+  bool find_objects_;
+  std::vector<grasping_msgs::Object> objects_;
+  std::vector<grasping_msgs::Object> supports_;
+
+  ros::Subscriber cloud_sub_;
+  ros::Publisher object_cloud_pub_;
+  ros::Publisher support_cloud_pub_;
+
+  boost::shared_ptr<ShapeGraspPlanner> planner_;
+  boost::shared_ptr<ObjectSupportSegmentation> segmentation_;
+
+  boost::shared_ptr<server_t> server_;
+
+  pcl::PassThrough<pcl::PointXYZRGB> range_filter_;
+};
+
+}  // namespace simple_grasping
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "basic_grasping_perception");
+  ros::NodeHandle n("~");
+  simple_grasping::BasicGraspingPerception perception_n_planning(n);
+  ros::spin();
+  return 0;
+}
---
+++ src/shape_grasp_planner.cpp
@@ -0,0 +1,273 @@
+/*
+ * Copyright 2013-2014, Unbounded Robotics Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Unbounded Robotics, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Author: Michael Ferguson
+
+#include <Eigen/Eigen>
+#include <ros/ros.h>
+#include <simple_grasping/shape_grasp_planner.h>
+
+using shape_msgs::SolidPrimitive;
+
+namespace simple_grasping
+{
+
+trajectory_msgs::JointTrajectory makeGraspPosture(double pose)
+{
+  trajectory_msgs::JointTrajectory trajectory;
+  trajectory.joint_names.push_back("l_gripper_finger_joint");
+  trajectory.joint_names.push_back("r_gripper_finger_joint");
+  trajectory_msgs::JointTrajectoryPoint point;
+  point.positions.push_back(pose/2.0);
+  point.positions.push_back(pose/2.0);
+  point.effort.push_back(28.0);
+  point.effort.push_back(28.0);
+  point.time_from_start = ros::Duration(10.0);
+  trajectory.points.push_back(point);
+  return trajectory;
+}
+
+
+moveit_msgs::GripperTranslation makeGripperTranslation(std::string frame,
+                                                       double min,
+                                                       double desired,
+                                                       double x_axis = 1.0,
+                                                       double y_axis = 0.0,
+                                                       double z_axis = 0.0)
+{
+  moveit_msgs::GripperTranslation translation;
+  translation.direction.vector.x = x_axis;
+  translation.direction.vector.y = y_axis;
+  translation.direction.vector.z = z_axis;
+  translation.direction.header.frame_id = frame;
+  translation.min_distance = min;
+  translation.desired_distance = desired;
+  return translation;
+}
+
+
+Eigen::Quaterniond quaternionFromEuler(float yaw, float pitch, float roll)
+{
+  float sy = sin(yaw*0.5);
+  float cy = cos(yaw*0.5);
+  float sp = sin(pitch*0.5);
+  float cp = cos(pitch*0.5);
+  float sr = sin(roll*0.5);
+  float cr = cos(roll*0.5);
+  float w = cr*cp*cy + sr*sp*sy;
+  float x = sr*cp*cy - cr*sp*sy;
+  float y = cr*sp*cy + sr*cp*sy;
+  float z = cr*cp*sy - sr*sp*cy;
+  return Eigen::Quaterniond(w,x,y,z);
+}
+
+
+ShapeGraspPlanner::ShapeGraspPlanner(double gripper_max_opening,
+                                     double gripper_finger_depth) :
+    max_opening_(gripper_max_opening),
+    finger_depth_(gripper_finger_depth)
+{
+}
+
+int ShapeGraspPlanner::createGrasp(const geometry_msgs::PoseStamped& pose,
+                                   double gripper_pitch,
+                                   double x_offset,
+                                   double z_offset,
+                                   double quality)
+{
+  moveit_msgs::Grasp grasp;
+  grasp.grasp_pose = pose;
+
+  // defaults
+  grasp.pre_grasp_posture = makeGraspPosture(max_opening_);
+  grasp.grasp_posture = makeGraspPosture(0.0);
+  grasp.pre_grasp_approach = makeGripperTranslation("wrist_roll_link", 0.1, 0.15);
+  grasp.post_grasp_retreat = makeGripperTranslation("wrist_roll_link", 0.1, 0.15, -1.0);
+
+  // initial pose
+  Eigen::Affine3d p = Eigen::Translation3d(pose.pose.position.x,
+                                           pose.pose.position.y,
+                                           pose.pose.position.z) *
+                        Eigen::Quaterniond(pose.pose.orientation.w,
+                                           pose.pose.orientation.x,
+                                           pose.pose.orientation.y,
+                                           pose.pose.orientation.z);
+  // translate by x_offset, 0, z_offset
+  p = p * Eigen::Translation3d(x_offset, 0, z_offset);
+  // rotate by 0, pitch, 0
+  p = p * quaternionFromEuler(0.0, gripper_pitch, 0.0);
+  // apply grasp point -> wrist_roll offset
+  p = p * Eigen::Translation3d(-0.160, 0, 0);
+
+  grasp.grasp_pose.pose.position.x = p.translation().x();
+  grasp.grasp_pose.pose.position.y = p.translation().y();
+  grasp.grasp_pose.pose.position.z = p.translation().z();
+  Eigen::Quaterniond q = (Eigen::Quaterniond)p.linear();
+  grasp.grasp_pose.pose.orientation.x = q.x();
+  grasp.grasp_pose.pose.orientation.y = q.y();
+  grasp.grasp_pose.pose.orientation.z = q.z();
+  grasp.grasp_pose.pose.orientation.w = q.w();
+
+  grasp.grasp_quality = quality;
+
+  grasps_.push_back(grasp);
+  return 1;
+}
+
+// Create the grasps going in one direction around an object
+// starts with gripper level, rotates it up
+// this works for boxes and cylinders
+int ShapeGraspPlanner::createGraspSeries(
+  const geometry_msgs::PoseStamped& pose,
+  double depth, double width, double height,
+  bool use_vertical)
+{
+  int count = 0;
+
+  // Gripper opening is limited
+  if (width >= (max_opening_*0.9))
+    return count;
+
+  // Depth of grasp calculations
+  double x = depth/2.0;
+  double z = height/2.0;
+  if (x > finger_depth_)
+    x = finger_depth_ - x;
+  if (z > finger_depth_)
+    z = finger_depth_ - z;
+
+  // Grasp along top of box
+  for (double step = 0.0; step < depth/2.0; step += 0.1)
+  {
+    if (use_vertical)
+      count += createGrasp(pose, 1.57, step, -z, 1.0 - 0.1*step);  // vertical
+    count += createGrasp(pose, 1.07, step, -z + 0.01, 0.7 - 0.1*step);  // slightly angled down
+    if (step > 0.05)
+    {
+      if (use_vertical)
+        count += createGrasp(pose, 1.57, -step, -z, 1.0 - 0.1*step);
+      count += createGrasp(pose, 1.07, -step, -z + 0.01, 0.7 - 0.1*step);
+    }
+  }
+
+  // Grasp horizontally along side of box
+  for (double step = 0.0; step < height/2.0; step += 0.1)
+  {
+    count += createGrasp(pose, 0.0, x, step, 0.8 - 0.1*step);  // horizontal
+    count += createGrasp(pose, 0.5, x-0.01, step, 0.6 - 0.1*step);  // slightly angled up
+    if (step > 0.05)
+    {
+      count += createGrasp(pose, 0.0, x, -step, 0.8 - 0.1*step);
+      count += createGrasp(pose, 0.5, x-0.01, -step, 0.6 - 0.1*step);
+    }
+  }
+
+  // A grasp on the corner of the box
+  count += createGrasp(pose, 1.57/2.0, x - 0.005, -z + 0.005, 0.25);
+
+  return count;
+}
+
+int ShapeGraspPlanner::plan(const grasping_msgs::Object& object,
+                            std::vector<moveit_msgs::Grasp>& grasps)
+{
+  ROS_INFO("shape grasp planning starting...");
+
+  // Need a shape primitive
+  if (object.primitives.size() == 0)
+  {
+    // Shape grasp planner can only plan for objects
+    //  with SolidPrimitive bounding boxes
+    return -1;
+  }
+
+  if (object.primitive_poses.size() != object.primitives.size())
+  {
+    // Invalid object
+    return -1;
+  }
+
+  // Clear out internal vector
+  grasps_.clear();
+
+  // Setup Pose
+  geometry_msgs::PoseStamped grasp_pose;
+  grasp_pose.header = object.header;
+  grasp_pose.pose = object.primitive_poses[0];
+
+  // Setup object orientation
+  Eigen::Quaterniond q(object.primitive_poses[0].orientation.w,
+                       object.primitive_poses[0].orientation.x,
+                       object.primitive_poses[0].orientation.y,
+                       object.primitive_poses[0].orientation.z);
+
+  // Setup object dimensions
+  double x, y, z;
+  if (object.primitives[0].type == SolidPrimitive::BOX)
+  {
+    x = object.primitives[0].dimensions[SolidPrimitive::BOX_X];
+    y = object.primitives[0].dimensions[SolidPrimitive::BOX_Y];
+    z = object.primitives[0].dimensions[SolidPrimitive::BOX_Z];
+  }
+  else if (object.primitives[0].type == SolidPrimitive::CYLINDER)
+  {
+    x = y = 2.0 * object.primitives[0].dimensions[SolidPrimitive::CYLINDER_RADIUS];
+    z = object.primitives[0].dimensions[SolidPrimitive::CYLINDER_HEIGHT];
+  }
+
+  // Generate grasps
+  for (int i = 0; i < 4; ++i)
+  {
+    grasp_pose.pose.orientation.x = q.x();
+    grasp_pose.pose.orientation.y = q.y();
+    grasp_pose.pose.orientation.z = q.z();
+    grasp_pose.pose.orientation.w = q.w();
+
+    if (i < 2)
+    {
+      createGraspSeries(grasp_pose, x, y, z);
+    }
+    else
+    {
+      // Only two sets of unqiue vertical grasps
+      createGraspSeries(grasp_pose, x, y, z, false);
+    }
+
+    // Next iteration, rotate 90 degrees about Z axis
+    q = q * quaternionFromEuler(-1.57, 0.0, 0.0);
+    std::swap(x, y);
+  }
+
+  ROS_INFO("shape grasp planning done.");
+
+  grasps = grasps_;
+  return grasps.size();  // num of grasps
+}
+
+}  // namespace simple_grasping
