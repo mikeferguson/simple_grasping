5f6b7ab0a0ad634669c44470452cd5e2a2e438e1
Michael Ferguson <mfergs7@gmail.com>
2023-12-16 12:49:48 -0500

    check in crap on ubr

--- scripts/do_perception.py
+++ scripts/do_perception.py
@@ -48,9 +48,30 @@ class PerceptionClient(Node):
         goal = FindGraspableObjects.Goal()
         goal.plan_grasps = True
         self.action_client.wait_for_server()
-        self.action_client.send_goal_async(goal)
+        self._goal_future = self.action_client.send_goal_async(goal)
+        self._goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected')
+            return
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        for surface in result.support_surfaces:
+            self.get_logger().info('{}'.format(surface.header))
+            self.get_logger().info('Surface: {}'.format(surface.surface))
+        for object in result.objects:
+            self.get_logger().info('{}'.format(object.object.header))
+            self.get_logger().info('Object on {}: {}'.format(object.object.support_surface, object.object.primitive_poses))
+        rclpy.shutdown()
 
 if __name__=="__main__":
     rclpy.init()
     client = PerceptionClient()
     client.run()
+    rclpy.spin(client)
